653861c64be4a660b0c9a683e7e17927319dfb88
Khumbolawo Mussa <khumbolawo87@gmail.com>
2024-08-14 17:45:29 +0200

    test: :white_check_mark: Pass all tests in 1st exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function (yourString, repitions) {
+  let stringArr = [];
+  let reps = repitions;
+  if (reps < 0) {
+    reps = "ERROR";
+    return reps;
+  } else {
+    reps = repitions;
+  }
 
+  for (i = 0; i < reps; i++) {
+    stringArr.push(yourString);
+  }
+  let newString = stringArr.join("");
+  console.log(newString);
+  return newString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(yourString) {
+    
 };
 
 // Do not edit below this line
